e95605181eee12b83df96384eeb543d5822dbbe9
borisdvlpr <eduffmiranda@hotmail.com>
2026-04-22 23:54:02 +0100

    feat(main): add application entrypoint and invoke crawler execution

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	crawler "github.com/borisdvlpr/fs-crawler-go/cmd"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	if err := crawler.Run(); err != nil {
+		slog.Error("server exited with error", "error", err)
+		os.Exit(1)
+	}
 }
