65edef7edc574a6e9428ce46d63a7db58623da40
borisdvlpr <eduffmiranda@hotmail.com>
2026-04-23 23:28:22 +0100

    feat(file): add ReadFile function in file module to read and return file contents

--- internal/file/file.go
+++ internal/file/file.go
@@ -1,6 +1,8 @@
 package file
 
 import (
+	"fmt"
+	"io"
 	"os"
 )
 
@@ -10,4 +12,18 @@ func CreateDir(dirName string) error {
 	}
 
 	return nil
+}
+
+func ReadFile(filePath string) ([]byte, error) {
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0444)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+	return data, nil
 }
