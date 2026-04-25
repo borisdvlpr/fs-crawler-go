757f8b6c5c881c86dd878ed0da1e95a660e9d8b6
borisdvlpr <eduffmiranda@hotmail.com>
2026-04-25 01:11:38 +0100

    style(main): fix logging on main file

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 
 	if err := crawler.Run(); err != nil {
-		slog.Error("server exited with error", "error", err)
+		slog.Error("application exited with error", "error", err)
 		os.Exit(1)
 	}
 }
