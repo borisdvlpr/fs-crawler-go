970603a6c23cb6efd7fee7885a3975fe37e779d3
borisdvlpr <eduffmiranda@hotmail.com>
2026-04-25 01:14:04 +0100

    feat(cmd): initialize logging and load folder configuration

--- cmd/crawler.go
+++ cmd/crawler.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/borisdvlpr/fs-crawler-go/internal/file"
+	"github.com/borisdvlpr/fs-crawler-go/internal/log"
 )
 
 func Run() error {
@@ -11,6 +16,20 @@ func Run() error {
 
 	if err := os.MkdirAll("logs", os.ModePerm); err != nil {
 		return err
+	}
+
+	if err := log.SetupLogs(); err != nil {
+		return err
+	}
+
+	fileData, err := file.ReadFile("./folders.txt")
+	if err != nil {
+		return err
+	}
+
+	paths := strings.Split(string(fileData), ";")
+	if len(paths) == 0 {
+		return fmt.Errorf("paths file is empty")
 	}
 
 	return nil
