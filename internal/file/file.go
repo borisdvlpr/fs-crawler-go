497fdb996a8fe74112c36dbae3d8b70fbb077dcd
borisdvlpr <eduffmiranda@hotmail.com>
2026-04-27 23:09:48 +0100

    feat(file): add FileEntry and FilesList types in file package for file metadata

--- internal/file/file.go
+++ internal/file/file.go
@@ -6,6 +6,16 @@ import (
 	"os"
 )
 
+type FileEntry struct {
+	Path string `json:"path"`
+	Size int64  `json:"size"`
+	LMod int64  `json:"lmod"`
+}
+
+type FilesList struct {
+	Files []FileEntry `json:"files"`
+}
+
 func ReadFile(filePath string) ([]byte, error) {
 	file, err := os.OpenFile(filePath, os.O_RDONLY, 0444)
 	if err != nil {
