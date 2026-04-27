6c92c2117568b3cc7c45d92705ec61e6dfd9e92b
borisdvlpr <eduffmiranda@hotmail.com>
2026-04-27 23:28:01 +0100

    feat(file): add recursive folder reader to collect file metadata

--- internal/file/file.go
+++ internal/file/file.go
@@ -3,7 +3,9 @@ package file
 import (
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
+	"path/filepath"
 )
 
 type FileEntry struct {
@@ -29,4 +31,36 @@ func ReadFile(filePath string) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+func readFolder(folderPath string, fileList *FilesList) error {
+	entries, err := os.ReadDir(folderPath)
+	if err != nil {
+		return fmt.Errorf("failed to read dir %s: %w", folderPath, err)
+	}
+
+	for _, entry := range entries {
+		fullPath := filepath.Join(folderPath, entry.Name())
+
+		if entry.IsDir() {
+			if err := readFolder(fullPath, fileList); err != nil {
+				slog.Error("error reading subfolder", "path", fullPath, "err", err)
+			}
+			continue
+		}
+
+		info, err := entry.Info()
+		if err != nil {
+			slog.Error("error reading file info", "path", fullPath, "err", err)
+			continue
+		}
+
+		fileList.Files = append(fileList.Files, FileEntry{
+			Path: fullPath,
+			Size: info.Size(),
+			LMod: info.ModTime().Unix(),
+		})
+	}
+
+	return nil
 }
